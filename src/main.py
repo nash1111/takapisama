f42f0137280a1d9d030d576ba587d2e90177348b
nash1111 <ken1251213@gmail.com>
2022-07-25 17:22:21 +0900

    WIP

--- src/main.py
+++ src/main.py
@@ -5,3 +5,9 @@ app = FastAPI()
 @app.get("/")
 def read_root():
   return {"Hello": "World"}
+
+def main():
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+
+if __name__ == "__main__":
+    main()
---
+++ src/start.sh
@@ -0,0 +1,1 @@
+. $HOME/.poetry/env && poetry run uvicorn main:app --reload --host 0.0.0.0 --port 9004
